f096523e41a620b04d4531bad047545a0df66208
Seongwan <waniboyy@gmail.com>
2024-05-09 21:47:18 +0900

    master init

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+import pandas as pd
+
+with open('vqa_train_questions.json') as f:
+    question_data = json.load(f)
+with open('vqa_train_annotations.json') as f:
+    answer_data = json.load(f)
+
+print((question_data['questions'][:3]))
+print()
+print((answer_data['annotations'][0]))
