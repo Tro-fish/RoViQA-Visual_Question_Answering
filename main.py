dd6cca36f8c807923da05705ccdefef702a75b28
Seongwan <waniboyy@gmail.com>
2024-05-22 21:44:13 +0900

    new presentation

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
 import pandas as pd
 
-with open('vqa_train_questions.json') as f:
+with open('datasets/vqa_train_questions.json') as f:
     question_data = json.load(f)
-with open('vqa_train_annotations.json') as f:
+with open('datasets/vqa_train_annotations.json') as f:
     answer_data = json.load(f)
 
 print((question_data['questions'][:3]))
