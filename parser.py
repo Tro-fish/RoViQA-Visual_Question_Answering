6e446ed600a3e61bece8d6366d76a95de2e46119
wani <waniboyy@gmail.com>
2024-06-14 13:49:43 +0900

    ADD required

--- parser.py
+++ parser.py
@@ -2,10 +2,10 @@ import argparse
 
 def train_parse_args():
     parser = argparse.ArgumentParser(description="Train VQA Model")
-    parser.add_argument('--train_annotation_path', type=str, default='/home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/train.csv', help="Path to the training data CSV file")
-    parser.add_argument('--test_annotation_path', type=str, default='/home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/test.csv', help="Path to the test data CSV file")
-    parser.add_argument('--train_img_path', type=str, default='/home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/image/train', help="Path to the training images directory")
-    parser.add_argument('--test_img_path', type=str, default='/home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/image/test', help="Path to the test images directory")
+    parser.add_argument('--train_annotation_path', type=str, required=True, default='/home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/train.csv', help="Path to the training data CSV file")
+    parser.add_argument('--test_annotation_path', type=str, required=True, default='/home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/test.csv', help="Path to the test data CSV file")
+    parser.add_argument('--train_img_path', type=str,required=True, default='/home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/image/train', help="Path to the training images directory")
+    parser.add_argument('--test_img_path', type=str,required=True, default='/home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/image/test', help="Path to the test images directory")
     parser.add_argument('--lr', type=float, default=5e-5, help="Learning rate")
     parser.add_argument('--batch_size', type=int, default=112, help="Batch size")
     parser.add_argument('--num_epochs', type=int, default=5, help="Number of epochs")
@@ -15,13 +15,7 @@ def train_parse_args():
 def test_parser_args():
     parser = argparse.ArgumentParser(description="Inference VQA Model")
     
-    parser.add_argument('--vqa_model_path', type=str, default='Trofish/RoViQA', help="Path to the trained model file")
-    parser.add_argument('--image_path', type=str, default='/home/wani/Desktop/Visual-Question-Answering/images/apple_image.jpg', help="Path to the image file")
-    parser.add_argument('--annotation_text', type=str, default='What is the name of the fruit in the picture?', help="Annotation text for the image")
+    parser.add_argument('--vqa_model_path', type=str,required=True, default='Trofish/RoViQA', help="Path to the trained model file")
+    parser.add_argument('--image_path', type=str, required=True,default='images/apple_image.jpg', help="Path to the image file")
+    parser.add_argument('--annotation_text', type=str,required=True, default='What is the name of the fruit in the picture?', help="Annotation text for the image")
     return parser.parse_args()
-
-# /home/wani/Desktop/Temp/Visual-Question-Answering/datasets/dacon/image/test/test_00003.jpg
-# What color are the cabinets?
-
-# /home/wani/Desktop/Visual-Question-Answering/images/apple_image.jpg
-# What is the name of the fruit in the picture?
